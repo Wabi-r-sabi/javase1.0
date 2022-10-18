c202e89d93af9c469985856cf4b072c559e0e074
hacker2889 <3331497264@qq.com>
2022-10-18 15:04:57 +0800

    pkg

---
+++ src/com/qiang/Dog.java
@@ -0,0 +1,13 @@
+package com.qiang;
+
+/**
+ * @descriptions: Dog
+ * @author: 一只强
+ * @date: 2022/10/16 10:17
+ * @version: 1.0
+ */
+public class Dog {
+    public static void main(String[] args) {
+
+    }
+}
