5f107f9779d74b57934ce824634fae578fe2f4ee
hacker2889 <3331497264@qq.com>
2022-10-18 15:05:44 +0800

    pkg

---
+++ src/com/use/Test.java
@@ -0,0 +1,18 @@
+package com.use;
+
+import com.ming.Dog;
+
+/**
+ * @descriptions: Test
+ * @author: 一只强
+ * @date: 2022/10/16 10:30
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        System.out.println(dog);
+        com.qiang.Dog dog1 = new com.qiang.Dog();
+        System.out.println(dog1);
+    }
+}
