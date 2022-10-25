f0e9981ce8aeb616f2fccf68395eda4510984779
hacker2889 <3331497264@qq.com>
2022-10-25 21:33:13 +0800

    modifier

---
+++ src/com/line/modifier/Test.java
@@ -0,0 +1,16 @@
+package com.line.modifier;
+
+/**
+ * @descriptions: Test
+ * @author: 一只强
+ * @date: 2022/10/16 12:36
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        Aa aa = new Aa();
+        aa.test();
+        Bb bb = new Bb();
+        bb.say();
+    }
+}
