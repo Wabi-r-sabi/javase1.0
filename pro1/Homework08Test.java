931db205e141f75d90b36251c6fa05298e975260
hacker2889 <3331497264@qq.com>
2022-10-27 16:27:37 +0800

    ResumeTest

---
+++ pro1/Homework08Test.java
@@ -0,0 +1,26 @@
+package Homework;
+
+/**
+ * @descriptions: RectangleDome
+ * @author: 一只强
+ * @date: 2022/10/27 14:21
+ * @version: 1.0
+ */
+/*
+public class RectangleDome {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        Rectangle rectangle1 = new Rectangle(20);
+        Rectangle rectangle3 = new Rectangle(20, 30);
+        rectangle3.showInfo();
+    }
+}
+*/
+
+public class Homework08Test {
+    public static void main(String[] args) {
+        Resume resume = new Resume();
+        Resume resume1 = new Resume("一只强", "男", 20);
+        resume1.introduce();
+    }
+}
